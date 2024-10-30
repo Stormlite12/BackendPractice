80e3bd62d12230436031052747182fe390881232
Siddartha Datta <sid2002official@gmail.com>
2024-10-30 20:00:30 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ const express = require('express')
 const app = express()
 const port =4000
 
+const githubData= {
+    "login": "Stormlite12",
+    "id": 68964321,
+    "node_id": "MDQ6VXNlcjY4OTY0MzIx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/68964321?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Stormlite12",
+    "html_url": "https://github.com/Stormlite12",
+    "followers_url": "https://api.github.com/users/Stormlite12/followers",
+    "following_url": "https://api.github.com/users/Stormlite12/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Stormlite12/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Stormlite12/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Stormlite12/subscriptions",
+    "organizations_url": "https://api.github.com/users/Stormlite12/orgs",
+    "repos_url": "https://api.github.com/users/Stormlite12/repos",
+    "events_url": "https://api.github.com/users/Stormlite12/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Stormlite12/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Siddartha Datta",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 5,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 1,
+    "created_at": "2020-07-29T16:50:08Z",
+    "updated_at": "2024-10-30T14:14:52Z"
+  }
+
+app.get('/github', (req,res) =>{
+    res.json(githubData)
+})
+
 app.get('/',(req,res) =>{
     res.send('Hello World!')
 })
